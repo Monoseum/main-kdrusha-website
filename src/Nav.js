05e050de701f53d87779f44c24e789fbc42379aa
Brandon Powell <brandonpdesigns@gmail.com>
2017-03-24 14:37:22 -0400

    Add Files To Get Github

---
+++ src/Nav.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import logo from './logo.svg';
+import './style.css';
+
+class Nav extends React.Component {
+  render() {
+    return (
+      <div className="wrapper">
+        <div className="navbar">
+          <img src={logo} className="logo" alt="logo" />
+
+          <ul className="header">
+             <li><a href="#">Home</a></li>
+             <li><a href="#">News</a></li>
+             <li><a href="#">Events</a></li>
+             <li><a href="#">Shop</a></li>
+          </ul>
+
+          <div className="social-media-icons">
+            <li><a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
+            <li><a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
+            <li><a href="#"><i class="fa fa-instagram" aria-hidden="true"></i></a></li>
+            <li><a href="#"><i class="fa fa-spotify" aria-hidden="true"></i></a></li>
+            <li><a href="#"><i class="fa fa-soundcloud" aria-hidden="true"></i></a></li>
+            <li><a href="#"><i class="fa fa-youtube" aria-hidden="true"></i></a></li>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Nav;
---
+++ src/Nav.test.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Nav from './Nav';
+
+it('renders without crashing', () => {
+  const navbar = document.createElement('navbar');
+  ReactDOM.render(<App />, div);
+});
